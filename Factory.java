1ab0c817b8c45a5d3fb71a93a2f60ffb54a19fbe
idont-code <pveznu10@gmail.com>
2025-11-22 05:43:27 -0800

    Implemented Functionality for Factory

--- Factory.java
+++ Factory.java
@@ -4,6 +4,9 @@ public class Factory extends Buildings
 {
     GreenfootImage factory = new GreenfootImage("factory.png");
     private boolean doorCreated = false;
+    private int repairAmount = 5;
+    private int cooldown = 50;    
+    private int timer = 0;
     
     public Factory()
     {
@@ -11,21 +14,33 @@ public class Factory extends Buildings
         factory.scale(300,200);
         setImage(factory);
         
-   
-        
     }
     
     
     public void act()
     {
         if(!doorCreated)
-        {
-            
-            
-            
+        {   
             FactoryDoor factorydoor = new FactoryDoor();
             getWorld().addObject(factorydoor,getX() + 30,getY() + 50);
             doorCreated = true;
         }
+        
+        if (timer >= cooldown) 
+        {
+            repairRobots();
+            timer = 0;
+        }
     }
+    
+    private void repairRobots() {
+        if (getWorld() == null) return;
+        for (Robot r : getWorld().getObjects(Robot.class)) {
+            if (r.getHealth() < r.maxHealth) {
+                int newHealth = Math.min(r.getHealth() + repairAmount, r.maxHealth);
+                r.setHealth(newHealth);
+            }
+        }
+    }
+
 }
